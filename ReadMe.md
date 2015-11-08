66b45d501f8a925b4dd20436c7eb6453e8f49b47
Paul M. Bendixen <paulbendixen@gmail.com>
2015-11-08 01:08:59 +0100

    Added a ReadMe.md and commented on the source code

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+#Århus C++ User Group date finder
+Some people have complained that the date of the next aacppug meeting can be somewhat difficult to figure out.
+
+Well as so many other things in computing, it's easy once you have a program to figure it out for you.
+
+The date is chosen in order to facilitate the use of [OSAA](http://osaa.dk)'s rooms as a backup for our meetings; our meetings are placed on the wedensday of the week after the board meeting. Since the board meeting is always on the tuesday following tech talk tuesday (which is the first tuesday of each month), the aacppug meeting will regularly be planned on the wedensday following the third tuesday in each mont.
+
+This very simple rule is what is expressed in this small program.
+
+It is implemented using Howard Hinnants date library, which he presented at CPPCON 2015 and has made available here on github.
+
+The license of this program extends of course only as far as my own code, the code written by Howard is his and is distributed under his license.
--- src/aacppugDateFinder.cpp
+++ src/aacppugDateFinder.cpp
@@ -1,12 +1,22 @@
-#include "date.h"
-#include <iostream>
+/* This is Free Software (C) Paul M. Bendixen licensed under the GPL v. 2.0, see the license file */
+#include "date.h"	// The date library is Howard Hinnants
+#include <iostream>	// Yes, we still print text to the terminal as if it were the dark ages
 
+/**
+ * Calculate the date of the next Århus C++ User Group meeting. Make it possible to extend using
+ * argc/argv to input a month of interest*/
 int main( int argc, char* argv[] )
 {
+	// Using namespace should not be done
 	namespace d = date;
+	// Get the current date so that we can calculate the date of the meeting
 	auto today = d::year_month_day{ d::floor< d::days >( std::chrono::system_clock::now() ) };
+	// Get the day of the first regular tuesday hack of OSAA
 	auto hackDay = d::tue[ 3 ]/today.month()/today.year();
+	// The day of the meeting will regularly be the day after that
 	auto cppDay = d::year_month_day{ d::day_point( hackDay ) + d::days{ 1 } };
+
+	// You english... What's wrong with just appending a .?
 	std::string ending;
 	switch( unsigned( cppDay.day() ) % 10 )
 	{
@@ -22,6 +32,7 @@ int main( int argc, char* argv[] )
 		default:
 			ending = "th";
 	};
+	// Finally print out the string of the next meeting
 	std::cout << "The Aarhus Cpp user group should convene the " << cppDay.day() << ending << " of this month.\n";
 
 }
